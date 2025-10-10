84ba99c78d7877cfb55130323d387c4d4a0fa26c
Shashank Yadav <shashank.y@getlokalapp.com>
2025-10-10 16:06:13 +0530

    Changed the emailjs config to true

--- src/config/emailConfig.js
+++ src/config/emailConfig.js
@@ -6,13 +6,13 @@
 
 const emailConfig = {
   // Set to true to enable automatic email sending, false to disable
-  ENABLE_EMAIL_SENDING: false,
+  ENABLE_EMAIL_SENDING: true,
 
   // Recipient email address
   RECIPIENT_EMAIL: 'yadavshashank700@gmail.com',
 
   // Send email automatically when results are shown (no button click required)
-  AUTO_SEND_ON_RESULTS: false,
+  AUTO_SEND_ON_RESULTS: true,
 };
 
 export default emailConfig;
